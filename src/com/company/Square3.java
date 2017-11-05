59973fcc961aadc55db83e760a7b6c69581dec49
mcastill1234 <30333120+mcastill1234@users.noreply.github.com>
2017-11-05 08:18:11 -0500

    Basic print, math functions and practice problems. Added conditionals.

---
+++ src/com/company/Square3.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Square3 {
+    public static void printSquare(double x){
+        System.out.println(x*x);
+    }
+
+    public static void main(String[] arguments){
+        printSquare(5);
+    }
+}
